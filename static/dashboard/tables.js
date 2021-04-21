26e20e35f115af87a75e1db536963df72a138932
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-21 16:04:46 +0100

    dashboard.templates dashboard tables

---
+++ static/dashboard/tables.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(document).ready(function () {
+        $('#machinesTable').DataTable({
+            "lengthMenu": [ 5, 10, 25 ]
+        });
+        $('#workersTable').DataTable({
+            "lengthMenu": [ 5, 10, 25 ]
+        });
+        $('#latestChangesTableAddRemoved').DataTable({
+            "lengthMenu": [ 4, 10, 25 ],
+            "searching": false,
+            "lengthChange": false
+        });
+        $('#latestChangesTableUpdates').DataTable({
+            "lengthMenu": [ 4, 10, 25 ],
+            "searching": false,
+            "lengthChange": false
+        });
+        $('#latestChangesTableFixedVulns').DataTable({
+            "lengthMenu": [ 4, 10, 25 ],
+            "searching": false,
+            "lengthChange": false
+        });
+    });
+});
