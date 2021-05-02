6b154d48896f44ffc7d8a69dd39aa23fc79105f0
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-02 11:54:58 +0100

    Merge branch 'main' into dev/docker

--- secureuall/login/models.py
+++ secureuall/login/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserType(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+
+class SecureuallUser(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+        related_name='secureuallUser',
+    )
+    userType = models.ForeignKey(UserType, on_delete=models.CASCADE, related_name='users')
+
+    def __str__(self):
+        return self.name
