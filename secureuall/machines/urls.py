b2e1172036f9336256abe1f868321e81decb33e4
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-02 12:19:43 +0100

    secureuall.machines suspending kafka test because it was giving problems

--- secureuall/machines/urls.py
+++ secureuall/machines/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import kafka_test
+# from .views import kafka_test
 
 app_name = 'machines'
 
 urlpatterns = [
-    path('kafka', kafka_test, name='kafka_test')
+    # path('kafka', kafka_test, name='kafka_test')
 ]
