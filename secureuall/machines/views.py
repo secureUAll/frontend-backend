3b65324e25a07fd6e7970e777fb8f26212378062
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-05 15:53:14 +0100

    Merge corrections

--- secureuall/machines/views.py
+++ secureuall/machines/views.py
@@ -3,9 +3,19 @@ from kafka import KafkaProducer
 from django.http import HttpResponse
 import logging
 import json
+import machines.dataContext as dataContext
 
 logging.basicConfig(level=logging.DEBUG)
 # Create your views here.
+context = dataContext.machineContext
+
+def MachinesView(request, *args, **kwargs):
+    return render(request, "machines/machines.html", context)
+
+
+def RequestsView(request, *args, **kwargs):
+    return render(request, "machines/requests.html", context)    
+
 
 def kafka_test(request):
     producer = KafkaProducer(bootstrap_servers='kafka:9092',                            
