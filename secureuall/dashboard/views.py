c3c3f69288eaf6ab387dcf425156307109ce2a3b
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-08 22:02:43 +0100

    secureuall.dashboard made workers table interactive

--- secureuall/dashboard/views.py
+++ secureuall/dashboard/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.decorators import login_required
 
+from workers.models import Worker
+
 # Create your views here.
 @login_required
 def DashboardView(request, *args, **kwargs):
-    return render(request, "dashboard/dashboard.html", {})
+    return render(request, "dashboard/dashboard.html", {
+        'workers': Worker.objects.all().order_by('-created')
+    })
