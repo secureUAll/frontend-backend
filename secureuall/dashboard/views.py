42af34d94f02af0751836d95db9e308a16e99dad
Isadora <flisadora@gmail.com>
2021-05-10 02:22:03 +0100

    Revert "Merge remote-tracking branch 'origin/dev/machinesPageInteractive' into dev/machinesPageInteractive"
    This reverts commit 45003f033f66754cc58abaa1b23bb8ab4da8d08b.

--- secureuall/dashboard/views.py
+++ secureuall/dashboard/views.py
@@ -3,13 +3,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.decorators import login_required
 
-from workers.models import Worker
-from machines.models import Machine
-
 # Create your views here.
 @login_required
 def DashboardView(request, *args, **kwargs):
-    return render(request, "dashboard/dashboard.html", {
-        'workers': Worker.objects.all().order_by('-created'),
-        'machines': Machine.objects.all()
-    })
+    return render(request, "dashboard/dashboard.html", {})
