15891433019c2c51ca31934e4461c74489b4a5ba
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-20 10:38:08 +0100

    login.models for UserAccessRequest

--- secureuall/login/models.py
+++ secureuall/login/models.py
@@ -13,3 +13,16 @@ class User(AbstractUser):
         if not user or not user.is_authenticated:
             return False
         return user.is_admin or user.machines.all().count()
+
+
+class UserAccessRequest(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accessRequest')
+    motive = models.TextField()
+    machines = models.TextField()  # Separated by semicollon
+    created_at = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+    pending = models.BooleanField(default=True)
+    notes = models.TextField()
+
+    def get_machines(self):
+        return self.machines.split(";")
