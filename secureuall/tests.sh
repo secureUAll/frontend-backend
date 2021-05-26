37a3c1041a6a278669c0e6e4bb9c8a2b0ef09dd9
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-26 17:06:10 +0100

    script for tests

---
+++ secureuall/tests.sh
@@ -0,0 +1,24 @@
+# Create new venv
+DIR="./venv"
+if [ ! -d "$DIR" ]; then
+  # If venv does not exist, create it
+  echo "venv does not exist, creating it..."
+  virtualenv -p python venv
+  source venv/bin/activate
+  pip install -r requirements.txt
+else
+  # Activate venv
+  source venv/bin/activate
+fi
+
+# Set up Django
+echo
+echo "Django set up..."
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+# Run local server
+echo
+echo "Running Django server..."
+python manage.py test
