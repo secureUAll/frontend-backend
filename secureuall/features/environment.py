37adb91cc9ce3c1ac1068c8517a90fba15516edd
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-06-18 18:20:26 +0100

    Revert "removed behave tests (temporarily)"
    This reverts commit ae7884196ddb4bc6a4b81637b1d611d3f2b3ea4a.

---
+++ secureuall/features/environment.py
@@ -0,0 +1,21 @@
+from behave import fixture, use_fixture
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
+import os
+
+
+@fixture
+def browser_firefox(context, timeout=30, **kwargs):
+    # Set up selenium
+    # If env variable is set, run on headless mode (because of GitHub actions)
+    opts = Options()
+    if os.environ.get('behaveHeadless', False):
+        opts.headless = True
+    context.browser = WebDriver(options=opts)
+    context.browser.implicitly_wait(10)
+    # Schedule clean up for when tests end
+    context.add_cleanup(context.browser.quit)
+
+
+def before_all(context):
+    use_fixture(browser_firefox, context)
